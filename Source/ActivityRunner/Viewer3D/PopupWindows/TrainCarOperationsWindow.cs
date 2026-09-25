de400ed80751630525aaca90467d3a30bde79943
Agustin Luzardo <101438505+agustinluzardo@users.noreply.github.com>
2026-09-25 02:07:54 -0300

    Use correct wagon type namespace

--- Source/ActivityRunner/Viewer3D/PopupWindows/TrainCarOperationsWindow.cs
+++ Source/ActivityRunner/Viewer3D/PopupWindows/TrainCarOperationsWindow.cs
@@ -10,7 +10,7 @@ using GetText;
 
 using Microsoft.Xna.Framework;
 
-using Orts.Formats.Msts.Models;
+using Orts.Formats.Msts;
 using Orts.Simulation;
 using Orts.Simulation.Physics;
 using Orts.Simulation.RollingStocks;
